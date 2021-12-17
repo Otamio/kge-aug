a5d617618c88b4e7f2ae685a7ccc29129ef08e5b
Otamio <ca.john.wang@gmail.com>
2021-12-17 12:46:24 -0800

    fix naming issues

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def main():
 
             import json
             from tqdm import tqdm
-            with open(f"datasets/{dataset}/stats/{target_train.split('.')[0]}.json") as fd:
+            with open(f"datasets/{dataset}/stats/{target_train.split('.')[0].replace('_Chain', '')}.json") as fd:
                 medians = json.load(fd)
 
             for i, row in tqdm(test.iterrows()):
